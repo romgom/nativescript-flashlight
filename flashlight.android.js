0fa578e417049b27208f60541f0430ab24395560
Rom Gomez <remdez@gmail.com>
2016-11-17 22:11:15 +0800

    Update flashlight.android.js
    change "camera" to "android.hardware.Camera" in setFlashMode as it causing a js runtime error
    assigned null value to camera after released.

--- flashlight.android.js
+++ flashlight.android.js
@@ -13,15 +13,19 @@ flashlight.on = function() {
 		camera = android.hardware.Camera.open(0);
 		parameters = camera.getParameters();
 	}
-	parameters.setFlashMode(camera.Parameters.FLASH_MODE_TORCH);
+	//parameters.setFlashMode(camera.Parameters.FLASH_MODE_TORCH);  //original code 
+	//change "camera" to "android.hardware.Camera" in setFlashMode as it causing a js runtime error
+	parameters.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_TORCH);
 	camera.setParameters(parameters);
 	camera.startPreview();
 };
 flashlight.off = function() {
-	parameters.setFlashMode(camera.Parameters.FLASH_MODE_OFF);
+	//parameters.setFlashMode(camera.Parameters.FLASH_MODE_OFF); //original code 
+	parameters.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_OFF);
 	camera.setParameters(parameters);
 	camera.stopPreview();
 	camera.release();
+	camera = null; //added to fully release the camera
 };
 
 module.exports = flashlight;
